e5bde8c95df2e393aa2c1d05d5188c68897c15b5
Daniel <dsecanas254@gmail.com>
2021-03-14 14:06:02 -0600

    Probando codigo en java fx
    Se agregaron algunas lineas temporales para familiarizarse con javafx y sus características

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,22 +1,72 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.Group;
+import javafx.scene.Parent;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontPosture;
+import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
+import javafx.scene.control.Button;
+import javafx.scene.text.Text;
+import javafx.scene.Node;
+
+import java.io.FileInputStream;
+
 
 public class Main extends Application {
-
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
+    public void start(Stage Lobby) throws Exception{
+        //Se crea una imagen asi-------------------------------------------------------------------------
+        FileInputStream input = new FileInputStream("Imagenes/START.png");
+        Image imagen=new Image(input);
+        ImageView img = new ImageView(imagen);
+        //----------------------------------------------------------------------------------------
+
+        Button b1=new Button();
+        Lobby.setTitle( "Space Invaders" );
+
+        b1.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("Hola mundo");
+            }
+        });
+
+
+
+        b1.setLayoutX(100);
+        b1.setLayoutY(300);
+
+        Group root = new Group(); //se crea la ventana
+
+        b1.setGraphic(img); //Se le da al boton la imagen
+        b1.setWrapText(true); //La verdad no se
+
+
+        root.getChildren().add(b1); //crea el boton el canvas
+        Scene scene = new Scene( root,800,600 ); //se liga scene al root
+        Lobby.setScene( scene ); //se le da al metodo el scene
+
+        //Creacion de texto en el canvas
+        Text texto = new Text();
+        texto.setText("Hola mundo");
+        texto.setX(50);
+        texto.setY(50);
+        root.getChildren().add(texto); //add children siempre se usa para agregar un nodo
+
+        Lobby.show();
     }
-
-
     public static void main(String[] args) {
         launch(args);
     }
