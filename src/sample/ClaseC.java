33c23ed2d224f04d2a9fd57d19f039ee4c11e4a5
Daniel <dsecanas254@gmail.com>
2021-03-22 20:34:47 -0600

    Lista circular progreso
    Metodos Funcionales al 100%:
    -addLast
    -addFirst
    -leer
    -obtenerDato
    Falta:
    -eraseFirst
    -eraseLast
    -erasePos

--- src/sample/ClaseC.java
+++ src/sample/ClaseC.java
@@ -7,6 +7,7 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 
 public class ClaseC {
+    static ListaCircular l = new ListaCircular();
     public static void IniciarClase(Group juego) throws FileNotFoundException {
         Image ufo1 = new Image(new FileInputStream("Imagenes/UFO1.png"));
         ImageView nave1 = new ImageView();
@@ -19,6 +20,12 @@ public class ClaseC {
         nave2.setX(500);
         nave2.setY(100);
         juego.getChildren().add(nave1);
+        l.addFirst(25);
+        l.addFirst(36);
+        l.addFirst(66);
+        //l.addLast(95);
+        l.leer();
+        l.obtenerDato(2);
 
 
 
--- src/sample/ListaCircular.java
+++ src/sample/ListaCircular.java
@@ -13,8 +13,8 @@ class Nodo{
     }
 }
 public class ListaCircular {
+    int largo=0;
     private Nodo first;
-    private Nodo prev;
     private Nodo last;
     public ListaCircular(){
         this.first=null;
@@ -22,41 +22,82 @@ public class ListaCircular {
     }
     public void addFirst(int valor){
         if (this.first==null){
-            this.first= new Nodo(valor);
-            this.last=this.first;
+            this.first = new Nodo(valor);
+            this.last = this.first;
+            this.first.next = this.last;
+            this.last.prev = this.first;
+            largo+=1;
+
         }else {
-        Nodo current= new Nodo(valor);
-        current.next=this.first;
-        this.first.prev=current;
-        this.first=current;
-        this.last=this.first;
+            Nodo current= new Nodo(valor);
+            current.next=this.first;
+            this.first.prev=current;
+            this.first=current;
+            this.last.next=this.first;
+            this.first.prev= this.last;
+            largo+=1;
         }
     }
-    public void addLast(int dato){
-        if(this.first==null){
-            this.first=new Nodo(dato);
-            this.last=this.first;
+    public void addLast(int dato) {
+        if (this.first == null) {
+            this.first = new Nodo(dato);
+            this.last = this.first;
+            this.first.next = this.last;
+            this.last.prev = this.first;
+            largo+=1;
         }else{
             Nodo current = this.first;
             while(current.next != this.first){
-                current = current.next;
+                current= current.next;
             }
-            Nodo tmp = this.last;
+            Nodo tmp=this.last;
             Nodo ant = tmp;
-            tmp.next = new Nodo(dato);
+            tmp.next= new Nodo(dato);
             tmp=tmp.next;
             tmp.prev=ant;
             this.last=tmp;
+            this.last.next=this.first;
         }
     }
     public void leer(){
         Nodo current = this.first;
-        if(this.first==this.last){
+        if(this.first==null){
             System.out.println("Vacia");
         }
-        while (current != this.last){
+        int i=0;
+        while (i != largo){
             System.out.println(current.get_value());
             current=current.next;
+            i+=1;
         }
+    }
+    public int obtenerDato(int posicion) {
+        Nodo current = this.first;
+        if (this.first == null) {
+            return 0;
+        }
+        int i = 0;
+        int res = 0;
+        while (i != largo) {
+            if ((i + 1) == posicion) {
+                res = current.get_value();
+                break;
+            } else {
+                current = current.next;
+                i += 1;
+            }
+        }
+        System.out.println(res);
+        return res;
+
+    }
+    public void eraseFirst(){
+
+    }
+    public void eraseLast(){
+
+    }
+    public void erasePos(int posicion){
+
     }
 }
