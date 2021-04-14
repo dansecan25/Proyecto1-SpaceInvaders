f9e6f238ce0e576fbd96b88d23d216ef727a258b
Daniel <dsecanas254@gmail.com>
2021-04-14 15:29:58 -0600

    Cambios
    Sin importancia

--- src/main/java/proyecto1/ClaseC.java
+++ src/main/java/proyecto1/ClaseC.java
@@ -14,42 +14,42 @@ public class ClaseC {
     private static boolean alive = true;
     public static void IniciarClaseC(Group juego) throws FileNotFoundException {
         currentClass.setClass("C", l);
-        new NaveEnemiga(110, 100, juego, l);
-        new NaveEnemiga(220, 100, juego, l);
-        new NaveEnemiga(330, 100, juego, l);
-        new NaveEnemiga(440, 100, juego, l);
-        new NaveEnemiga(550, 100, juego, l);
+        new NaveEnemiga(110, 100, juego);
+        new NaveEnemiga(220, 100, juego);
+        new NaveEnemiga(330, 100, juego);
+        new NaveEnemiga(440, 100, juego);
+        new NaveEnemiga(550, 100, juego);
         var naveBoss = l.obtenerDato(3);
-        NaveEnemiga.toBoss((ImageView) naveBoss);
-
-        Task<Void> vivo = new Task<Void>() {
-            @Override
-            protected Void call() throws Exception {
-                int i = 0;
-                while(i<10){
-                    if(i==3){
-                        alive=false;
-                        break;
-                    }
-                    i+=1;
-                    try {
-                        Thread.sleep(2000);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
-                return null;
-            }
-        };
-        vivo.setOnSucceeded(event -> {
-            if(!alive){
-                double pos=Math.random()*l.tamanoLista();
-                int posicion =  (int) pos;
-                var nodo =l.obtenerDato(posicion);
-                NaveEnemiga.toBoss((ImageView) nodo);
-            }
-        });
-        new Thread(vivo).start();
+        //NaveEnemiga.toBoss((ImageView) naveBoss);
+//
+//        Task<Void> vivo = new Task<Void>() {
+//            @Override
+//            protected Void call() throws Exception {
+//                int i = 0;
+//                while(i<10){
+//                    if(i==3){
+//                        alive=false;
+//                        break;
+//                    }
+//                    i+=1;
+//                    try {
+//                        Thread.sleep(2000);
+//                    } catch (InterruptedException e) {
+//                        e.printStackTrace();
+//                    }
+//                }
+//                return null;
+//            }
+//        };
+//        vivo.setOnSucceeded(event -> {
+//            if(!alive){
+//                double pos=Math.random()*l.tamanoLista();
+//                int posicion =  (int) pos;
+//                var nodo =l.obtenerDato(posicion);
+//                //NaveEnemiga.toBoss((ImageView) nodo);
+//            }
+//        });
+//        new Thread(vivo).start();
     }
 
 }
